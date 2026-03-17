ccce5b33e2cae66f368dac4a5cea33cffe57c044
vanshr2 <vansh.rohilla0209@gmail.com>
2026-03-17 13:57:54 -0500

    made random changes

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -33,7 +33,7 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
         if num2 != 0:
             return num1 / num2
         else:
-            raise ValueError("Cannot divide by zero.")
+            raise ValueError("Cannot divide by zero. dchesvbfrvusehbitgrehuvvngfidnjsznfjkbcmxojnuc4ywremex")
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
